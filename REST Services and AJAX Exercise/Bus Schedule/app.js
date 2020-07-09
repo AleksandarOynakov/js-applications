a69b5ef324263145f50b2b927110bd2e6e49e294
AleksandarOynakov <58415669+AleksandarOynakov@users.noreply.github.com>
2020-07-09 23:35:45 +0300

    No Judge assignment but fully working

---
+++ REST Services and AJAX Exercise/Bus Schedule/app.js
@@ -0,0 +1,59 @@
+function solve() {
+    let currentState = 'stopped';
+    let stopID = 'depot';
+    let nextStopName = '';
+    let nextStopID = '';
+    let infoBox = document.getElementById('info').children[0];
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
+
+    function toggle(){
+        departBtn.disabled = !departBtn.disabled;
+        arriveBtn.disabled = !arriveBtn.disabled;
+    }
+
+    function depart() {
+        currentState = 'moving';
+        toggle();
+        sendRequest();
+    }
+
+    function arrive() {
+        currentState = 'stopped';
+        toggle();
+        infoBox.textContent = `Arriving at ${nextStopName}`;
+    }
+
+    function sendRequest() {
+        let url = `https://judgetests.firebaseio.com/schedule/${stopID}.json`;
+        fetch(url)
+            .then((response) => {
+                if (response.status !== 200) {
+                    throw new Error('Request error');
+                }
+                return response.json();
+            })
+            .then((data) => dataHandler(data))
+            .catch(() => {
+                infoBox.textContent = 'Error';
+                departBtn.disabled = true;
+                arriveBtn.disabled = true;
+            });
+    }
+
+    function dataHandler(data) {
+        nextStopName = data.name;
+        nextStopID = data.next;
+        if (currentState === 'moving') {
+            infoBox.textContent = `Next stop ${nextStopName}`;
+        }
+        stopID = nextStopID;
+    }
+
+    return {
+        depart,
+        arrive
+    };
+}
+
+let result = solve();
---
+++ REST Services and AJAX Exercise/Bus Stop/app.js
@@ -0,0 +1,31 @@
+function getInfo() {
+    let stopName = document.getElementById('stopName');
+    let busesUL = document.getElementById('buses');
+    let stopID = document.getElementById('stopId').value;
+
+    fetch(`https://judgetests.firebaseio.com/businfo/${stopID}.json`)
+        .then(response => {
+            if (response.status !== 200) {
+                throw new Error('Request error');
+            }
+            return response.json();
+        })
+        .then(dataJSON => displayData(dataJSON))
+        .catch(err => displayError(err));
+
+
+    function displayData(dataJSON) {
+        busesUL.innerHTML = '';
+        stopName.textContent = dataJSON.name;
+        for (const key in dataJSON.buses) {
+            let li = document.createElement('li');
+            li.textContent = `Bus ${key} arrives in ${dataJSON.buses[key]}`;
+            busesUL.appendChild(li);
+        }
+    }
+
+    function displayError(err) {
+        busesUL.innerHTML = '';
+        stopName.textContent = 'Error';
+    }
+}
